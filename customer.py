b5beede8d79f2753b12e33a277f4080736a60a26
Rodney <rodney.amani@student.moringaschool.com>
2025-05-17 08:42:08 +0300

    add Customer class with name validation

---
+++ customer.py
@@ -0,0 +1,15 @@
+class Customer:
+    def __init__(self, name):
+        self._name = None
+        self.name = name 
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        else:
+            raise ValueError("Name must be a string between 1 and 15 characters")
---
+++ debug.py
@@ -0,0 +1,4 @@
+from customer import Customer
+
+c1 = Customer("Roddy")
+print(c1.name)
