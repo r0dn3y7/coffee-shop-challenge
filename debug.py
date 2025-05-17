9b95a30a982d987dadeea119383c253fc772db0f
Rodney <rodney.amani@student.moringaschool.com>
2025-05-17 12:09:03 +0300

    add Order class with validation and relationships debug

--- debug.py
+++ debug.py
@@ -3,8 +3,23 @@ from customer import Customer
 c1 = Customer("Roddy")
 print(c1.name)
 
-
+########
 from coffee import Coffee
 
 latte = Coffee("Latte")
 print(latte.name)
+
+
+########
+from customer import Customer
+from coffee import Coffee
+from order import Order
+
+c1 = Customer("Alice")
+c2 = Customer("Bob")
+latte = Coffee("Latte")
+mocha = Coffee("Mocha")
+
+order1 = Order(c1, latte, 4.5)
+order2 = Order(c2, latte, 5.0)
+
