88ce44fe174ee39239ae53927879f181d599dd81
Rodney <rodney.amani@student.moringaschool.com>
2025-05-18 07:52:50 +0300

    customer.py changes

--- coffee.py
+++ coffee.py
@@ -9,4 +9,25 @@ class Coffee:
     def name(self):
         return self._name  
     
-    
+    from order import Order
+
+def orders(self):
+    return [order for order in Order._all if order.coffee == self]
+
+def customers(self):
+    return list({order.customer for order in self.orders()})
+
+def num_orders(self):
+    return len(self.orders())
+
+def average_price(self):
+    orders = self.orders()
+    if not orders:
+        return 0
+    return sum(order.price for order in orders) / len(orders)
+
+Coffee.orders = orders
+Coffee.customers = customers
+Coffee.num_orders = num_orders
+Coffee.average_price = average_price
+
--- customer.py
+++ customer.py
@@ -1,7 +1,10 @@
 class Customer:
+    all = []
+
     def __init__(self, name):
         self._name = None
-        self.name = name 
+        self.name = name
+        Customer.all.append(self)
 
     @property
     def name(self):
@@ -12,20 +15,10 @@ class Customer:
         if isinstance(value, str) and 1 <= len(value) <= 15:
             self._name = value
         else:
-            raise ValueError("Name must be a string between 1 and 15 characters")
+            raise ValueError("Name must be a string between 1 and 15 characters.")
 
+    def orders(self):
+        return [order for order in Order.all if order.customer == self]
 
-from order import Order
-
-def orders(self):
-    return [order for order in Order._all if order.customer == self]
-
-def coffees(self):
-    return list({order.coffee for order in self.orders()})
-
-def create_order(self, coffee, price):
-    return Order(self, coffee, price)
-
-Customer.orders = orders
-Customer.coffees = coffees
-Customer.create_order = create_order
+    def coffees(self):
+        return list({order.coffee for order in self.orders()})
